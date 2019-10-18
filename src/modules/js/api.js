82c67acf1a0df9d7f0db197eeca6baf0d691aab1
wyx8267 <369264448@qq.com>
2019-10-18 22:57:20 +0800

    重构首页推荐列表&底部栏组件化

---
+++ src/modules/js/api.js
@@ -0,0 +1,13 @@
+let url = {
+    hotLists: '/index/hotLists'
+}
+
+let host = 'http://rap2api.taobao.org/app/mock/7058'
+
+for (let key in url) {
+    if (url.hasOwnProperty(key)) {
+        url[key] = host + url[key]
+    }
+}
+
+export default url
