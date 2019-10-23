ab6e414d7bd72056090ff7a712fb9c458b7e557f
wyx8267 <369264448@qq.com>
2019-10-23 23:19:15 +0800

    完成商品详情页

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -4,7 +4,9 @@ let url = {
     topList: '/category/topList',
     subList: '/category/subList',
     rank: '/category/rank',
-    searchList: '/search/list'
+    searchList: '/search/list',
+    details: '/goods/details',
+    deal: '/goods/deal'
 }
 
 let host = 'http://rap2api.taobao.org/app/mock/7058'
