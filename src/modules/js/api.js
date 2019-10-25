c85abee7b596f065f3a32bcc5a5ab31f755738ca
wyx8267 <369264448@qq.com>
2019-10-25 21:11:05 +0800

    购物车状态1--选择状态完成

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -7,7 +7,11 @@ let url = {
     searchList: '/search/list',
     details: '/goods/details',
     deal: '/goods/deal',
-    addCart: '/cart/add'
+    addCart: '/cart/add',
+    cartLists: '/cart/list',
+    cartReduce: '/cart/reduce',
+    cartRemove: '/cart/remove',
+    cartMremove: '/cart/mremove'
 }
 
 let host = 'http://rap2api.taobao.org/app/mock/7058'
